2d9805d39ed0db5a10bb9f78ad0a723d0a703741
Jan Winkler <winkler@cs.uni-bremen.de>
2016-07-08 10:53:08 +0200

    Adapting reasoning package to ltfnp

--- ltfnp_reasoning/ltfnp_reasoning/src/main/java/org/knowrob/ltfnp_reasoning/LTFnPReasoner.java
+++ ltfnp_reasoning/ltfnp_reasoning/src/main/java/org/knowrob/ltfnp_reasoning/LTFnPReasoner.java
@@ -1,6 +1,6 @@
 /*
- * RackReasoner.java
- * Copyright (c) 2015 Jan Winkler
+ * LTFnPReasoner.java
+ * Copyright (c) 2016 Jan Winkler
  *
  * All rights reserved.
  *
@@ -14,9 +14,6 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Universitaet Bremen nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -40,7 +37,7 @@ import java.lang.Integer;
 import org.knowrob.utils.ros.RosUtilities;
 
 
-public class RackReasoner {
+public class LTFnPReasoner {
     /** 
      *  Determined whether the position [dPoseX, dPoseY, dPoseZ] is on
      *  the rack level whose position is [dRackLevelX, dRackLevelY,
--- ltfnp_reasoning/prolog/init.pl
+++ ltfnp_reasoning/prolog/init.pl
@@ -1,5 +1,5 @@
 /** 
-  Copyright (C) 2015 Jan Winkler <winkler@cs.uni-bremen.de>
+  Copyright (C) 2016 Jan Winkler <winkler@cs.uni-bremen.de>
   All rights reserved.
   
   Redistribution and use in source and binary forms, with or without
@@ -10,9 +10,6 @@
       * Redistributions in binary form must reproduce the above copyright
         notice, this list of conditions and the following disclaimer in the
         documentation and/or other materials provided with the distribution.
-      * Neither the name of the <organization> nor the
-        names of its contributors may be used to endorse or promote products
-        derived from this software without specific prior written permission.
   
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -36,8 +33,6 @@
 :- use_module(library('ltfnp_reasoning')).
 
 :- register_ros_package(iai_semantic_maps).
-:- owl_parser:owl_parse('package://ltfnp_models/owl/shopping_area.owl').
-:- owl_parser:owl_parse('package://ltfnp_models/owl/shopping_rack.owl').
 :- owl_parser:owl_parse('package://ltfnp_models/owl/objects.owl').
 :- owl_parser:owl_parse('package://iai_semantic_maps/owl/iai_maps_addons.owl').
 
