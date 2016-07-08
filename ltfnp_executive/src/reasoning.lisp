16b3e4c69ba89c710c58ae5691a91406eaeeb343
Jan Winkler <winkler@cs.uni-bremen.de>
2016-07-08 13:08:22 +0200

    Predicates for manipulating known objects

--- ltfnp_executive/src/reasoning.lisp
+++ ltfnp_executive/src/reasoning.lisp
@@ -24,5 +24,89 @@
 
 (in-package :ltfnp-executive)
 
-(defun get-defined-object-types ()
-  )
+;;;
+;;; Utility predicates
+;;;
+
+(defmacro with-first-prolog-vars-bound (vars prolog-query &body body)
+  "Evaluates the prolog query `prolog-query' and transforms variables `vars' via `body', returning the result."
+  `(with-vars-bound ,vars
+       (lazy-car
+        (json-prolog:prolog ,prolog-query))
+     ,@body))
+
+(defmacro with-prolog-vars-bound (vars prolog-query &body body)
+  "Lists all results from the prolog query `prolog-query', each being transformed by `body'. `vars' denotes all variables to make available in `body'."
+  `(force-ll
+    (lazy-mapcar
+     (lambda (bdgs)
+       (with-vars-bound ,vars bdgs
+         ,@body))
+     (json-prolog:prolog ,prolog-query))))
+
+(defun json-symbol->string (symbol)
+  "Converts `symbol' as returned from json-prolog to a lisp-usable string by trimming `|' characters at the beginning and the end."
+  (let* ((string-symbol (write-to-string symbol)))
+    (subseq string-symbol 2 (- (length string-symbol) 2))))
+
+(defun split-prolog-symbol (prolog-symbol &key (delimiter '\#))
+  "Splits the namespace from the symbol of a prolog identifier symbol `prolog-symbol'. The two parts must be delimited by the delimiter `delimiter'. Returns a values list, consisting of the symbol, and the namespace."
+  (let ((delimiter-position
+          (position delimiter prolog-symbol :test #'string=)))
+    (when delimiter-position
+      (values
+       (subseq prolog-symbol (1+ delimiter-position))
+       (subseq prolog-symbol 0 delimiter-position)))))
+
+(defun strip-prolog-string (symbol)
+  "Combines the functionality of `json-symbol->string' and `split-prolog-symbol', resulting in a namespace-less string representing the value of `symbol'."
+  (split-prolog-symbol (json-symbol->string symbol)))
+
+(defun add-prolog-namespace (symbol &key (namespace "http://knowrob.org/kb/knowrob.owl") (delimiter '\#))
+  "Concatenates a string that consists of the given `namespace', the `delimiter', and finally the `symbol'. The default namespace represents the base KnowRob OWL namespace, and the default delimiter is `#'."
+  (concatenate
+   'string
+   namespace
+   (json-symbol->string (write-to-string delimiter))
+   symbol))
+
+;;;
+;;; Object and object class related predicates
+;;;
+
+(defun object-classes ()
+  "Lists all subclasses of the KnowRob `LTFnPObject` class. The
+base-class itself does not count towards the enlisted classes."
+  (cpl:mapcar-clean
+   (lambda (class)
+     (let ((stripped-class (strip-prolog-string class)))
+       (unless (string= stripped-class "LTFnPObject")
+         stripped-class)))
+   (with-prolog-vars-bound (?class)
+      `("ltfnp_object_class" ?class)
+    ?class)))
+
+(defun object-class-p (class)
+  "Returns a boolean value denoting whether `class' is a valid shopping item class (i.e., a subclass of `LTFnPObject')."
+  (not (not (json-prolog:prolog `("ltfnp_object_class" ,(add-prolog-namespace class))))))
+
+(defun object-instances ()
+  (cpl:mapcar-clean
+   (lambda (class)
+     (let ((stripped-class (strip-prolog-string class)))
+       (unless (string= stripped-class "LTFnPObject")
+         stripped-class)))
+   (with-prolog-vars-bound (?class)
+      `("ltfnp_object" ?class)
+    ?class)))
+
+(defun instantiate-object (class)
+  (assert (object-class-p class)
+          ()
+          "Class `~a' is not a subclass of `LTFnP'." class)
+  (with-first-prolog-vars-bound (?instance)
+      `("ltfnp_add_object" ,(add-prolog-namespace class) ?instance)
+    (strip-prolog-string ?instance)))
+
+(defun delete-object (object-id)
+  (json-prolog:prolog `("ltfnp_remove_object" ,(add-prolog-namespace object-id))))
