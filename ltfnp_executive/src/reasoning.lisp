a8c076391b32676aca213aa8f5fad34af67aa084
Jan Winkler <winkler@cs.uni-bremen.de>
2016-07-25 13:43:17 +0200

    Properly read prolog return value for semantic handle data

--- ltfnp_executive/src/reasoning.lisp
+++ ltfnp_executive/src/reasoning.lisp
@@ -164,12 +164,19 @@ base-class itself does not count towards the enlisted classes."
      (get-semantic-handle-details handle))
    (with-prolog-vars-bound (?handle)
        `("ltfnp_class_semantic_handle" ,(add-prolog-namespace class) ?handle)
-     (strip-prolog-string (json-symbol->string ?handle)))))
+     (strip-prolog-string ?handle))))
+
+(defun stripped-symbol-name (sym)
+  (let ((sym-name (symbol-name sym)))
+    (if (and (equal (subseq sym-name 0 1) "'")
+             (equal (subseq sym-name (1- (length sym-name)) (length sym-name)) "'"))
+        (subseq sym-name 1 (- (length sym-name) 1))
+        sym-name)))
 
 (defun get-semantic-handle-details (handle)
   (with-first-prolog-vars-bound (?grasptype ?tx ?ty ?tz ?qw ?qx ?qy ?qz)
       `("ltfnp_semantic_handle_details" ,(add-prolog-namespace handle) ?grasptype ?tx ?ty ?tz ?qw ?qx ?qy ?qz)
-    `(,?grasptype ,?tx ,?ty ,?tz ,?qw ,?qx ,?qy ,?qz)))
+    `(,(stripped-symbol-name ?grasptype) ,?tx ,?ty ,?tz ,?qw ,?qx ,?qy ,?qz)))
 
 ;;;
 ;;; Location related reasoning functions (mostly for convenience)
