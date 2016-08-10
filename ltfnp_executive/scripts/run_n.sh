4ad89ecf4c977810636ad89d557a5cb46c67124c
Jan Winkler <winkler@cs.uni-bremen.de>
2016-08-10 08:57:43 +0200

    Forgot `.sh` when calling `kill_type.sh`

--- ltfnp_executive/scripts/run_n.sh
+++ ltfnp_executive/scripts/run_n.sh
@@ -4,8 +4,8 @@ N_COUNT=$1
 
 for (( i=0; i<${N_COUNT}; i++ )); do
     roslaunch ltfnp_executive ltfnp_automated.launch
-    ./kill_type mongodb_log
-    ./kill_type semrec
-    ./kill_type gzserver
-    ./kill_type gazebo
+    ./kill_type.sh mongodb_log
+    ./kill_type.sh semrec
+    ./kill_type.sh gzserver
+    ./kill_type.sh gazebo
 done
