cf9d99ac229b65b1976caef009baefc72b9f5ff1
Jan Winkler <winkler@cs.uni-bremen.de>
2016-08-10 09:28:22 +0200

    Moved killing of relevant processes into separate file

---
+++ ltfnp_executive/scripts/kill_all_relevant.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+rosrun ltfnp_executive scripts/kill_type.sh mongodb_log
+rosrun ltfnp_executive scripts/kill_type.sh semrec
+rosrun ltfnp_executive scripts/kill_type.sh gzserver
+rosrun ltfnp_executive scripts/kill_type.sh gazebo
+rosrun ltfnp_executive scripts/kill_type.sh /opt/ros/
+rosrun ltfnp_executive scripts/kill_type.sh nav_pcontroller
--- ltfnp_executive/scripts/run_n.sh
+++ ltfnp_executive/scripts/run_n.sh
@@ -4,10 +4,5 @@ N_COUNT=$1
 
 for (( i=0; i<${N_COUNT}; i++ )); do
     roslaunch ltfnp_executive ltfnp_automated.launch
-    rosrun ltfnp_executive mongodb_log
-    rosrun ltfnp_executive semrec
-    rosrun ltfnp_executive gzserver
-    rosrun ltfnp_executive gazebo
-    rosrun ltfnp_executive /opt/ros/
-    rosrun ltfnp_executive nav_pcontroller
+    rosrun ltfnp_executive kill_all_relevant.sh
 done
