dab2c44eb4f9014f70801176d32d974c81e8c9c6
Jan Winkler <winkler@cs.uni-bremen.de>
2016-08-10 14:41:48 +0200

    Greatly reduced debug output for killing processes

--- ltfnp_executive/scripts/kill_type.sh
+++ ltfnp_executive/scripts/kill_type.sh
@@ -3,9 +3,8 @@
 TERM=$1
 
 PIDS=`ps aux | grep ${TERM} | grep -v grep | awk '{print $2}'`
-echo "$1 = $PIDS"
+
 printf '%s\n' "$PIDS" | while IFS= read -r PID
 do
-    echo "Doing:     kill -s SIGKILL ${PID}"
-    kill -s SIGKILL ${PID}
+    kill -s SIGKILL ${PID} &>/dev/null 2>&1
 done
