21ceff8c9bcf4e2ba55b1f96a836f0ebf9c1d7d6
Jan Winkler <winkler@cs.uni-bremen.de>
2016-08-11 13:24:42 +0200

    Properly stitch together four perspective camera views

--- ltfnp_executive/scripts/package_log.sh
+++ ltfnp_executive/scripts/package_log.sh
@@ -19,18 +19,28 @@ if [ -d "current-experiment/scene_camera" ]; then
 	
 	# Mogrification can do more; its just not used very thoroughly
 	# at the moment.
-	mogrify -resize 800x800 "scene_camera_${camera_index}_*.jpg"
+	#mogrify -resize 800x800 "scene_camera_${camera_index}_*.jpg"
 	
 	# For stretching the individual frames
-	convert *.jpg -delay 1 -morph 1 mogrified_${camera_index}_%05d.jpg
+	#convert *.jpg -delay 1 -morph 1 mogrified_${camera_index}_%05d.jpg
 	
 	# Actually stitching them together to form a video
-	avconv -i "mogrified_${camera_index}_%05d.jpg" -r 30 -c:v libx264 -crf 20 -pix_fmt yuv420p ../preview_${camera_index}.mov
+	#avconv -i "mogrified_${camera_index}_%05d.jpg" -r 30 -c:v libx264 -crf 20 -pix_fmt yuv420p ../preview_${camera_index}.mov
+	avconv -i "scene_camera_${camera_index}_%05d.jpg" -r 30 -c:v libx264 -crf 20 -pix_fmt yuv420p ../preview_${camera_index}.mov
     done
     
     # Cleanup
     cd -
     rm -Rf current-experiment/scene_camera
+    
+    # Stitch together videos
+    cd current-experiment
+    avconv -i preview_1.mov -i preview_2.mov -filter_complex "[0:v:0]pad=iw*2:ih[bg]; [bg][1:v:0]overlay=w" preview_top.mov
+    avconv -i preview_3.mov -i preview_4.mov -filter_complex "[0:v:0]pad=iw*2:ih[bg]; [bg][1:v:0]overlay=w" preview_bottom.mov
+    avconv -i preview_top.mov -i preview_bottom.mov -filter_complex "[0:v:0]pad=iw:2*ih[bg]; [bg][1:v:0]overlay=0:main_h/2" preview.mov
+    
+    rm -Rf preview_1.mov preview_2.mov preview_3.mov preview_4.mov preview_top.mov preview_bottom.mov
+    cd -
 fi
 
 ./package.sh
