43f7fcb4444bd035a1c8d983923121b0fefa5005
Jan Winkler <winkler@cs.uni-bremen.de>
2016-08-26 10:39:10 +0200

    Per default, log camera images

--- ltfnp_executive/scripts/prep_mongodb.sh
+++ ltfnp_executive/scripts/prep_mongodb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mongo roslog --eval "db.dropDatabase()"
-rosrun mongodb_log mongodb_log /tf /logged_designators /logged_metadata
+rosrun mongodb_log mongodb_log /tf /logged_designators /logged_metadata /head_mount_kinect/rgb/image_raw
